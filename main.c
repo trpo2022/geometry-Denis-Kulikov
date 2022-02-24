eeee8ff15199015857176e17b43cf326d7eff5b5
Kulikov Denis <denis_kulikov_a_2003@mail.ru>
2022-02-25 05:25:34 +0700

    Add elements

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define N 10 // максимальное количество фигур
+
+struct circle {
+    double point[2];
+    double radius;
+};
 
 int main()
 {
+    int* figur = malloc(N * sizeof(int));
+    int count_exit = 0, num = 0;
+
+    struct circle circle_pos[N];
+
     return 0;
 }
